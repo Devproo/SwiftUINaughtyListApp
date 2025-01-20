febea1764ac5a2dea34c13845d2cd0d305404cba
ipeerless <ipeerless0@gmail.com>
2025-01-20 10:33:14 +0200

    Initial Commit

---
+++ SwiftUINaughtyListApp/SwiftUINaughtyListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINaughtyListAppApp.swift
+//  SwiftUINaughtyListApp
+//
+//  Created by ipeerless on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINaughtyListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
